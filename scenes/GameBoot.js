4a504708a45b9bbc6801e7305f019a4270d073ae
ajwhite00 <aaronjwhite00@gmail.com>
2023-03-28 20:26:44 -0400

    Rock functionality

--- scenes/GameBoot.js
+++ scenes/GameBoot.js
@@ -6,14 +6,55 @@ class GameBoot extends Phaser.Scene {
     preload(){
         this.load.image("backpack", "assets/backpack.png");
         this.load.image("rock", "assets/rock.png");
+        this.load.image("amethyst", "assets/amethyst.png");
+        this.load.image("diamond", "assets/diamond.png");
+        this.load.image("emerald", "assets/emerald.png");
+        this.load.image("ruby", "assets/ruby.png");
+        this.load.image("sapphire", "assets/sapphire.png");
+        this.load.image("topaz", "assets/topaz.png");
+
+        this.load.audio("rockHit", "assets/rockHit.mp3");
+        this.load.audio("rockHitBreak", "assets/rockHitBreak.mp3");
+        this.load.audio("reward", "assets/reward.mp3");
+        this.load.audio("error", "assets/error.mp3");
     }
 
     create(){
-        
+
         let playerStats = {
             backPackCapacity: 5,
+            currentItemCount: 0,
+            currentBackpackitems: [],
+            pickAxePower: 100,
         }
 
-        this.scene.start("mine", { playerStats });
+        let rewards = {
+            amethyst: {
+                name: "amethyst",
+                value: 10
+            },
+            diamond: {
+                name: "diamond",
+                value: 10
+            },
+            emerald: {
+                name: "emerald",
+                value: 10
+            },
+            ruby: {
+                name: "ruby",
+                value: 10
+            },
+            sapphire: {
+                name: "sapphire",
+                value: 10
+            },
+            topaz: {
+                name: "topaz",
+                value: 10
+            },
+        }
+
+        this.scene.start("mine", { playerStats, rewards });
     }
 }
