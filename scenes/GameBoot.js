8b3509a4680d10ea55101aca5e356b333f347188
Isaac Van Meter <ivanmeter28@gmail.com>
2023-03-30 14:19:36 -0400

    added new assets

--- scenes/GameBoot.js
+++ scenes/GameBoot.js
@@ -7,9 +7,17 @@ class GameBoot extends Phaser.Scene {
         this.load.image("backpack", "assets/backpack.png");
         this.load.image("shop", "assets/shop.png");
         this.load.image("cave", "assets/cave.png");
+        this.load.image("rock", "assets/rock.png");
+        this.load.image("sellButton", "assets/sell.png");
+        this.load.image("coins", "assets/coins.png");
     }
 
     create(){
-        this.scene.start("mine");
+        
+        let playerStats = {
+            backPackCapacity: 5,
+        }
+
+        this.scene.start("mine", { playerStats });
     }
 }
