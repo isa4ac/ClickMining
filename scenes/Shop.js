df17884bdb3c7bdaf062130e1a16f4f261444a44
Isaac Van Meter <ivanmeter28@gmail.com>
2023-04-20 15:48:44 -0400

    Upgrade progress save

--- scenes/Shop.js
+++ scenes/Shop.js
@@ -6,15 +6,20 @@ class Shop extends Phaser.Scene {
   init() {
     this.playerStats = DataManager.load('playerStats');
     this.gameStats = DataManager.load('gameStats');
+    this.PAPowerInterval = 100;
+    this.BackpackInterval = 5;
+    this.AMPowerInterval = 50;
+    this.AMTimeInterval = 100;
   }
 
   sellItems() {
     var backpackItems = this.playerStats.currentBackpackItems;
-    for (var i = backpackItems.length -1; i >= 0 ; i--) {
+    for (var i = backpackItems.length - 1; i >= 0; i--) {
       this.playerStats.coins = this.playerStats.coins + backpackItems[i].value;
       backpackItems.pop(i);
     }
     this.playerStats.currentBackpackItems = backpackItems;
+    // update coin labels function here
     DataManager.update('playerStats', this.playerStats);
   }
 
@@ -46,30 +51,71 @@ class Shop extends Phaser.Scene {
 
     return roman;
   }
-  
+
+  upgrade(key) {
+    // validate
+
+    // incrementUpgrade / take coins
+    incrementUpgrade(key);
+
+    // update labels, pickaxe power, coins
+  }
+
+  updateLabels() {
+
+  }
+
+  getPrice(key) {
+    if (key === "pickaxeUpgrade") {
+      let pickAxePower = this.playerStats.pickAxePower;
+      return (1.15*pickAxePower)/10
+    } else if (key === "backpackUpgrade") {
+      let backPackCapacity = this.playerStats.backPackCapacity;
+      return (1.15*backPackCapacity)*20
+    } else if (key === "autoMinerDamageUpgrade") {
+      let backPackCapacity = this.playerStats.backPackCapacity;
+    } else if (key === "autoMinerSpeedUpgrade") {
+
+    }
+  }
+
+  incrementUpgrade(key) {
+    if (key === "pickaxeUpgrade") {
+      this.playerStats.pickAxePower = this.playerStats.pickAxePower + this.PAPowerInterval;
+    } else if (key === "backpackUpgrade") {
+      this.playerStats.backPackCapacity = this.playerStats.backPackCapacity + this.BackpackInterval;
+    } else if (key === "autoMinerDamageUpgrade") {
+      this.playerStats.autoMinerDamage = this.playerStats.autoMinerDamage + this.AMPowerInterval;
+    } else if (key === "autoMinerSpeedUpgrade") {
+      this.playerStats.autoMinerSpeed = this.playerStats.autoMinerSpeed - this.AMTimeInterval;
+    }
+    this.playerStats.coins = this.playerStats.coins - this.getPrice(key);
+  }
+
   getSublabel(key) {
     if (key === "pickaxeUpgrade") {
-      return `Pickaxe Power Will Become ${this.playerStats.pickAxePower + 100}`
+      return `+100 Pickaxe Power (${this.playerStats.pickAxePower + this.PAPowerInterval})`
     } else if (key === "backpackUpgrade") {
-      return `Backpack Capacity Will Become ${this.playerStats.backPackCapacity + 5}`
+      return `+5 Backpack Space (${this.playerStats.backPackCapacity + this.BackpackInterval})`
     } else if (key === "autoMinerDamageUpgrade") {
-      return `Autominer Will Deal ${this.playerStats.autoMinerDamage + 100} Damage`
+      return `+50 Auto Miner Power (${this.playerStats.autoMinerDamage + this.AMPowerInterval})`
     } else if (key === "autoMinerSpeedUpgrade") {
-      if (this.playerStats.autoMinerSpeed > 0) {
-        return `Autominer Will Strike Every ${(this.playerStats.autoMinerSpeed - 500) / 1000} Seconds`
+      if (this.playerStats.autoMinerSpeed >= 500) {
+        return `+0.1 Second Auto Miner Speed (${(this.playerStats.autoMinerSpeed - this.AMTimeInterval) / 1000}s)`
       } else {
-        return 'Max Autominer Speed Reached!'
+        return 'Max Autominer Speed Reached! (0.5s)'
       }
     }
   }
 
-    create(){
-        this.add.image(0, 50, 'shopBG').setOrigin(0);
-        const toolbar = new Toolbar(this);
-        this.add.toolbar;
+
+  create() {
+    this.add.image(0, 50, 'shopBG').setOrigin(0);
+    const toolbar = new Toolbar(this);
+    this.add.toolbar;
 
     // Create a new container for the menu
-    var menuContainer = this.add.container(150, 150);
+    var menuContainer = this.add.container(150, 140);
 
     // Define the row height and spacing
     var rowHeight = 60;
@@ -77,10 +123,10 @@ class Shop extends Phaser.Scene {
 
     // Define the data for the menu rows
     var menuData = [];
-    menuData.push({ label: `Pickaxe Upgrade Tier ${this.convertToRoman(((this.playerStats.pickAxePower) / 100) + 1)}`, key: "pickaxeUpgrade" }) // + 100 Per Tier
-    menuData.push({ label: `Backpack Upgrade Tier ${this.convertToRoman(((this.playerStats.backPackCapacity) / 5) + 1)}`, key: "backpackUpgrade" }) // + 5 Per Tier
-    menuData.push({ label: `Auto Miner Damage Tier ${this.convertToRoman(((this.playerStats.autoMinerDamage) / 100) + 1)}`, key: "autoMinerDamageUpgrade" }) // + 100 Per Tier
-    menuData.push({ label: `Auto Miner Speed Tier ${this.convertToRoman(((this.playerStats.autoMinerSpeed) / (-500)) + 12)}`, key: "autoMinerSpeedUpgrade" }) // - .5  Seconds Per Tier
+    menuData.push({ label: `Pickaxe Upgrade Tier ${this.convertToRoman(((this.playerStats.pickAxePower) / this.PAPowerInterval) + 1)}`, key: "pickaxeUpgrade" }) // + 100 Per Tier
+    menuData.push({ label: `Backpack Upgrade Tier ${this.convertToRoman(((this.playerStats.backPackCapacity) / this.BackpackInterval) + 1)}`, key: "backpackUpgrade" }) // + 5 Per Tier
+    menuData.push({ label: `Auto Miner Damage Tier ${this.convertToRoman(((this.playerStats.autoMinerDamage) / this.AMPowerInterval) + 1)}`, key: "autoMinerDamageUpgrade" }) // + 50 Per Tier
+    menuData.push({ label: `Auto Miner Speed Tier ${this.convertToRoman(((this.playerStats.autoMinerSpeed-this.AMTimeInterval) / 1000) + 1)}`, key: "autoMinerSpeedUpgrade" }) // - .1  Seconds Per Tier
 
     // Loop through the data and create a new row for each item
     for (var i = 0; i < menuData.length; i++) {
@@ -89,7 +135,7 @@ class Shop extends Phaser.Scene {
 
       // Add a background rectangle to the row
       // TO-DO make conditional statement if updgrade is available here
-      var rowBackground = this.add.rectangle(0, 0, 500, rowHeight, 0x333333);
+      var rowBackground = this.add.rectangle(0, 0, 500, rowHeight, 0x7e705d);
       rowBackground.setOrigin(0);
 
       // Set the row background as interactive and add a pointerdown event listener
@@ -101,63 +147,42 @@ class Shop extends Phaser.Scene {
       // Add the label text to the row
       var rowLabel = this.add.text(10, 10, menuData[i].label, { color: '#ffffff' });
 
-      var rowSublabel = this.add.text(10, 40, this.getSublabel(menuData[i].key), { color: '#c3c2c3' });
+      var rowSublabel = this.add.text(10, 35, this.getSublabel(menuData[i].key), { color: '#c3c2c3' });
       rowSublabel.setFontSize(14);
+
+      let buyButton = new Button(this, 425, rowHeight / 2, 'Buy', () => {
+        // upgrade logic ran here
+      });
 
       // Add the row elements to the row container
       rowContainer.add(rowBackground);
       rowContainer.add(rowLabel);
       rowContainer.add(rowSublabel);
+      rowContainer.add(buyButton);
 
       // Add the row container to the menu container
       menuContainer.add(rowContainer);
     }
 
-    let sellRowBackground = this.add.rectangle(0, 0, 500, rowHeight, 0x7ae08e);
+    let sellRowBackground = this.add.rectangle(0, 0, 500, rowHeight, 0x4c4438);
     sellRowBackground.setOrigin(0);
-    sellRowBackground.setInteractive();
-    sellRowBackground.on('pointerdown', () => {
-      // sell logic ran here
-      this.sellItems();
-    });
-    var sellRowContainer = this.add.container(0, (rowHeight + rowSpacing) * i);
+
+    var sellRowContainer = this.add.container(0, (rowHeight + rowSpacing + 10) * i);
     sellRowContainer.add(sellRowBackground);
     var sellRowLabel = this.add.text(10, 10, "Sell all resources", { color: '#ffffff' });
     sellRowContainer.add(sellRowLabel);
 
-    this.sellSound = this.sound.add('rockHit')
+    // Create the button and add it to the container
+    let sellButton = new Button(this, 425, rowHeight / 2, 'Sell', () => {
+      this.sellItems();
+    });
+    sellRowContainer.add(sellButton);
+
+    // this.sellSound = this.sound.add('rockHit')
 
     menuContainer.add(sellRowContainer);
     this.add.menuContainer;
   }
 
   update() { }
-}
-
-// 2. Create the button class
-class Button extends Phaser.GameObjects.Container {
-  constructor(scene, x, y, text) {
-    super(scene, x, y);
-
-    this.button = scene.add.image(0, 0, 'button');
-    this.buttonPressed = scene.add.image(0, 0, 'button_pressed').setVisible(false);
-    this.text = scene.add.text(0, 0, text, { fontSize: '24px', color: '#000' }).setOrigin(0.5);
-
-    this.add([this.button, this.buttonPressed, this.text]);
-
-    this.button.setInteractive({ useHandCursor: true });
-
-    // 3. Define the interactivity
-    this.button.on('pointerdown', () => {
-      this.button.setVisible(false);
-      this.buttonPressed.setVisible(true);
-    });
-
-    this.button.on('pointerup', () => {
-      this.button.setVisible(true);
-      this.buttonPressed.setVisible(false);
-    });
-
-    scene.add.existing(this);
-  }
 }
